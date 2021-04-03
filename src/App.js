2a69f907c0df61c5df552ef283313bac509079e8
Eddie Olivas <eddie@Eddies-MacBook-Pro.local>
2021-04-02 20:58:48 -0700

    Removed console log of process.env value.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/Burgerbuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
-console.log(process.env.REACT_APP_DB_URL);
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   render() {
@@ -14,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
             <Route path="/" component={BurgerBuilder} />
           </Switch>
         </Layout>
