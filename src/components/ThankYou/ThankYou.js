67083031b4fb6ac1edfbd327617bab0f5c3e0f75
eddieospaghettio <eddie@chrysaliswebdevelopment.com>
2021-04-17 15:46:54 -0700

    Added burger animations on load.

--- src/components/ThankYou/ThankYou.js
+++ src/components/ThankYou/ThankYou.js
@@ -10,12 +10,14 @@ import CanvasConfetti from '../UI/CanvasConfetti/CanvsConfetti';
 
 class ThankYou extends Component {
   render() {
-    const burger = this.props.ings ? <Burger className={classes.loadBurger} ingredients={this.props.ings} /> : <div className={classes.loadBurger}>TEST</div>;
+    const burger = this.props.ings
+      ? <Burger className={classes.loadBurger} ingredients={this.props.ings} /> 
+      : null;
     return (
       <div className={classes.ThankYou}>
         <h1>Sweet! You ordered a burger!</h1>
         <p>Here it is, we hope you enjoy it. Have an awesome day. <span role="img" aria-label="smiley emoji">&#128512;</span></p>
-        {burger}
+        <div className={classes.loadBurger}>{burger}</div>
         <Link to="/"><button className={classes.OrderAgainButton}>Order Another</button></Link>
         <Link to="/orders"><button className={classes.ViewOrdersButton}>View My Orders</button></Link>
         <CanvasConfetti></CanvasConfetti>
